3c8ce0053ea9fcec6a3c2a9dd6ca19ea7bc61f9d
ntp945812 <ntp945812@hotmail.com>
2025-06-03 10:34:46 +0800

    Separate the data content of DataTableView from UI updates.

--- data_table_view.py
+++ data_table_view.py
@@ -20,33 +20,36 @@ class DataTableView(DataTable):
         self.add_column(Text("主旨", justify="center"), width=50, key="title")
         self.add_column("來文單位", width=20, key="issue_unit")
         self.add_column("限辦日期", width=10, key="deadline")
-        self.add_data_rows()
+        self.load_documents()
         self.focus()
 
-    def add_data_rows(self):
+    def load_documents(self):
         for number, d in enumerate(self.documents, start=1):
+            row_data = (d.doc_type, d.issue_date, d.external_doc_number, d.internal_doc_number, d.title, d.issue_unit,
+                  d.deadline)
+            if (number-1,d.doc_id) in self.selected_docs:
+                row_data = (Text(s, style="grey89 on steel_blue") for s in row_data)
+
             self.add_row(
-                *(d.doc_type, d.issue_date, d.external_doc_number, d.internal_doc_number, d.title, d.issue_unit,
-                  d.deadline), label=str(number), height=None, key=d.doc_id)
+                *row_data, label=str(number), height=None, key=d.doc_id)
 
     def on_data_table_row_selected(self, message: DataTable.RowSelected):
 
         row_key = message.row_key
         row_index = message.cursor_row
-        data_table = message.data_table
 
         selected_row_metadata = (row_index, row_key.value)
 
         if selected_row_metadata not in self.selected_docs:
             self.selected_docs.append(selected_row_metadata)
-            for col in data_table.columns:
-                data_table.update_cell(row_key, col,
-                                       Text(data_table.get_cell(row_key, col), style="grey89 on steel_blue"))
+
         else:
             self.selected_docs.remove(selected_row_metadata)
-            for col in data_table.columns:
-                data_table.update_cell(row_key, col, data_table.get_cell(row_key, col).plain)
+        
+        self.reload_rows()
+        self.move_cursor(row=row_index)
 
-    def refresh_rows(self):
+    def reload_rows(self):
         self.clear()
+        self.load_documents()
 
